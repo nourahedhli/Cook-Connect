04f221ee56b7616ab3948faee691119c5be5595c
svp62 <svp62@njit.edu>
2021-04-08 11:21:04 -0400

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
       // An error happened.
     });
   }
- 
+
  
   loginWithGmail() {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,14 +11,6 @@ import {MatIconModule} from '@angular/material/icon';
 import {FormsModule} from '@angular/forms';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatListModule} from '@angular/material/list';
-
-
-
-
-
-
-
-
 
 @NgModule({
   declarations: [
