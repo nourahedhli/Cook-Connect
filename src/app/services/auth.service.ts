7bfa3b02519c91f7aa4277ad84715f4c7fa90a6a
svp62 <svp62@njit.edu>
2021-04-15 10:35:05 -0400

    sign up method created for service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import {AngularFireAuth} from '@angular/fire/auth'
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  isLoggedIn = false
+  constructor(public firebaseAuth : AngularFireAuth) { }
+  async signin(email: string, password : string){
+    await this.firebaseAuth.signInWithEmailAndPassword(email,password)
+    .then(res=>{
+      this.isLoggedIn = true
+      localStorage.setItem('user',JSON.stringify(res.user))
+    })
+  }
+  async signup(email: string, password : string){
+    await this.firebaseAuth.createUserWithEmailAndPassword(email,password)
+    .then(res=>{
+      this.isLoggedIn = true
+      localStorage.setItem('user',JSON.stringify(res.user))
+    })
+  }
 }
